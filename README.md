a2bc682000620223df05eb37c18cd179f4ec071c
Pankaj Vaghela <vpankaj1998official@gmail.com>
2020-08-06 22:46:11 +0530

    readme update, bug fixes

--- README.md
+++ README.md
@@ -1,7 +1,62 @@
 # react-native-use-otp-verify
 
-Hook for react-native-otp-verify
+Wrapper Hook for [react-native-otp-verify](https://github.com/faizalshap/react-native-otp-verify)
 
-Read basic of [react-native-otp-verify](https://github.com/faizalshap/react-native-otp-verify#readme) first
+Make yourself familiar with [react-native-otp-verify](https://github.com/faizalshap/react-native-otp-verify#readme) before using this hook .
 
-coming soon ...
+### Installation
+
+```bash
+yarn add react-native-otp-verify react-native-use-otp-verify
+```
+
+or
+
+```bash
+npm i react-native-otp-verify react-native-use-otp-verify
+```
+
+then Follow instruction of [react-native-otp-verify](https://github.com/faizalshap/react-native-otp-verify#readme) first
+
+### Usage
+
+#### Basic Example
+
+```js
+import React from 'react';
+import {useOtpVerify} from 'rn-use-otp-verify';
+
+const OtpConfirm = () => {
+  const [otp, setOtp] = React.useState('');
+  const [autoDetectedOtp, autoDetectError] =  useOtpVerify() ;
+
+  React.useEffect(() => {
+    if (autoDetectedOtp) {
+      setOtp(autoDetectedOtp);
+    }
+  }, [autoDetectedOtp, handleLogin]);
+
+  ...
+};
+
+```
+
+---
+
+### OTP Parser
+
+Default otp parser checks for following regex : /(\d+)[\s]_is|is[\s]_(\d+)\.?/g
+
+i.e. otp SMS must be one of following formats
+
+`OTP is 2098`
+
+`2098 is OTP`
+
+However, you can pass a custom `otpParser` to useOtpVerify hook.
+
+this `otpParser` must accept sms as argument and return otp as string or empty string.
+
+## LICENSE
+
+MIT
--- src/useOtpVerify.js
+++ src/useOtpVerify.js
@@ -2,22 +2,33 @@ import {useState, useEffect} from 'react';
 import {Platform} from 'react-native';
 import RNOtpVerify from 'react-native-otp-verify';
 
-export function smsListener(callback) {
-  RNOtpVerify.getOtp()
-    .then(p =>
-      RNOtpVerify.addListener(message => {
-        try {
-          if (message) {
-            callback(null, message);
-          }
-        } catch (error) {
-          callback(error);
-        }
-      }),
-    )
-    .catch(p => console.log(p));
+const ANDROID = 'android';
+const OTP_REGEX = /(\d+)[\s]*is|is[\s]*(\d+)\.?/g;
 
-  return () => RNOtpVerify.removeListener();
+export async function smsListener(callback) {
+  if (Platform.OS === ANDROID) {
+    RNOtpVerify.getOtp()
+      .then(p =>
+        RNOtpVerify.addListener(message => {
+          try {
+            if (message) {
+              callback(null, message);
+            }
+          } catch (error) {
+            callback(error);
+          }
+        }),
+      )
+      .catch(p => console.log(p));
+  } else {
+    callback(null, '');
+  }
+
+  return () => {
+    if (Platform.OS === ANDROID) {
+      RNOtpVerify.removeListener();
+    }
+  };
 }
 
 export function useSMSListener(smsHandler) {
@@ -40,24 +51,19 @@ export function useSMSListener(smsHandler) {
   return [sms, err];
 }
 
-const OTP_REGEX = /(\d+)[\s]*is|is[\s]*(\d+)\.?/g;
-
 function defaultOtpParser(sms) {
   let otpMatch = OTP_REGEX.exec(sms);
 
   return otpMatch ? otpMatch[1] || otpMatch[2] || '' : '';
 }
 
-//Custom OTP Parser must return an otp;
 export function useOtpVerify(otpParser = defaultOtpParser) {
   const [otp, setOTP] = useState('');
   const [sms, err] = useSMSListener();
 
   useEffect(() => {
-    if (Platform.OS === 'android') {
-      if (!err) {
-        setOTP(otpParser(sms));
-      }
+    if (!err) {
+      setOTP(otpParser(sms));
     }
   }, [err, otpParser, sms]);
 
