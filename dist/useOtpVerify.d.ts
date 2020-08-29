7220213a0563a5d78ec487f987e76277215ac91f
Pankaj Vaghela <vpankaj1998official@gmail.com>
2020-08-29 12:20:23 +0530

    switch to lib and index.ts

--- dist/useOtpVerify.d.ts
+++
@@ -1,7 +0,0 @@
-import RNOtpVerify from 'react-native-otp-verify';
-export declare function smsListener(callback: any): () => void;
-export declare function useSMSListener(smsHandler: any | undefined): (string | (() => void) | null)[];
-declare function defaultOtpParser(sms: string | null | any): string;
-export declare function useOtpVerify(otpParser?: typeof defaultOtpParser): (string | (() => void) | null)[];
-export default RNOtpVerify;
-//# sourceMappingURL=useOtpVerify.d.ts.map
