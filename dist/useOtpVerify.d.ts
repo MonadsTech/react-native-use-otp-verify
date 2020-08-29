65c779302ee5692291027a21faafd97603cbfa0b
Pankaj Vaghela <vpankaj1998official@gmail.com>
2020-08-29 12:07:13 +0530

    exports fixed

--- dist/useOtpVerify.d.ts
+++ dist/useOtpVerify.d.ts
@@ -4,3 +4,4 @@ export declare function useSMSListener(smsHandler: any | undefined): (string | (
 declare function defaultOtpParser(sms: string | null | any): string;
 export declare function useOtpVerify(otpParser?: typeof defaultOtpParser): (string | (() => void) | null)[];
 export default RNOtpVerify;
+//# sourceMappingURL=useOtpVerify.d.ts.map
